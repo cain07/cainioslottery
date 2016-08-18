32a3121fc33a74ac3c5ea949a485cc57cf0df25a
yaojianwei <yaojianwei@meicai.cn>
2016-08-18 15:42:56 +0800

    first

---
+++ lottery/lottery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lottery
+//
+//  Created by cain on 16/8/18.
+//  Copyright © 2016年 cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
